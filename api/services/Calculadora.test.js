19d68260e1d9b4f7a29c4d34d7bc73bf7b9b78db
Geovanna Menegasse <48860848+geovannamenegasse@users.noreply.github.com>
2022-07-04 16:57:45 -0300

    Update Calculadora.test.js

--- api/services/Calculadora.test.js
+++ api/services/Calculadora.test.js
@@ -1,77 +1,77 @@
 const calculadora = require("./Calculadora");
 
-describe('divisao', () => {
-    describe('2 números inteiros são passados como parâmetro ==> retorna a divisão de um pelo outro', () => {
-        test.each([
-            { numerador: 6,                       denominador: 3,                       retornoEsperado: 2 },
-            { numerador: 4,                       denominador: 2,                       retornoEsperado: 2 },
-            { numerador: -12,                     denominador: 3,                       retornoEsperado: -4 },
-            { numerador: -12,                     denominador: -3,                      retornoEsperado: 4 },
-            { numerador: 10,                      denominador: 3,                       retornoEsperado: 3.3333333333333335 },
-            { numerador: Number.MAX_SAFE_INTEGER, denominador: 5,                       retornoEsperado: 1801439850948198.2 },
-            { numerador: 1,                       denominador: Number.MAX_SAFE_INTEGER, retornoEsperado: 1.1102230246251568e-16 }
-        ])
-        ('%j', ({ numerador, denominador, retornoEsperado }) => {
-            expect(calculadora.divisao(numerador, denominador)).toBe(retornoEsperado);
-        });
-    });
+// describe('divisao', () => {
+//     describe('2 números inteiros são passados como parâmetro ==> retorna a divisão de um pelo outro', () => {
+//         test.each([
+//             { numerador: 6,                       denominador: 3,                       retornoEsperado: 2 },
+//             { numerador: 4,                       denominador: 2,                       retornoEsperado: 2 },
+//             { numerador: -12,                     denominador: 3,                       retornoEsperado: -4 },
+//             { numerador: -12,                     denominador: -3,                      retornoEsperado: 4 },
+//             { numerador: 10,                      denominador: 3,                       retornoEsperado: 3.3333333333333335 },
+//             { numerador: Number.MAX_SAFE_INTEGER, denominador: 5,                       retornoEsperado: 1801439850948198.2 },
+//             { numerador: 1,                       denominador: Number.MAX_SAFE_INTEGER, retornoEsperado: 1.1102230246251568e-16 }
+//         ])
+//         ('%j', ({ numerador, denominador, retornoEsperado }) => {
+//             expect(calculadora.divisao(numerador, denominador)).toBe(retornoEsperado);
+//         });
+//     });
 
-    describe('2 números são passados como parâmetro, e um deles ou ambos são float ==> retorna a divisão de um pelo outro', () => {
-        test.each([
-            { numerador: 7.5224455678,            denominador: 2.7666,                  retornoEsperado: 2.7190217479216368 },
-            { numerador: 7.5224455678,            denominador: -2,                      retornoEsperado: -3.7612227839 },
-            { numerador: Number. MAX_VALUE,       denominador: 4.2,                     retornoEsperado: 4.28022174967218e+307 },
-            { numerador: 8,                       denominador: Number. MAX_VALUE,       retornoEsperado: 4.450147717014404e-308 }
-        ])('%j', ({ numerador, denominador, retornoEsperado }) => {
-            expect(calculadora.divisao(numerador, denominador)).toBeCloseTo(retornoEsperado, 16);
-        });
-    });
+//     describe('2 números são passados como parâmetro, e um deles ou ambos são float ==> retorna a divisão de um pelo outro', () => {
+//         test.each([
+//             { numerador: 7.5224455678,            denominador: 2.7666,                  retornoEsperado: 2.7190217479216368 },
+//             { numerador: 7.5224455678,            denominador: -2,                      retornoEsperado: -3.7612227839 },
+//             { numerador: Number. MAX_VALUE,       denominador: 4.2,                     retornoEsperado: 4.28022174967218e+307 },
+//             { numerador: 8,                       denominador: Number. MAX_VALUE,       retornoEsperado: 4.450147717014404e-308 }
+//         ])('%j', ({ numerador, denominador, retornoEsperado }) => {
+//             expect(calculadora.divisao(numerador, denominador)).toBeCloseTo(retornoEsperado, 16);
+//         });
+//     });
 
-    describe('o numerador é um número maior que 0 mas o denominador é igual a 0 ==> retorna Infinity', () => {
-        test.each([
-            { numerador: 6                      },
-            { numerador: 2.1                    },
-            { numerador: Number. MAX_VALUE      },
-            { numerador: Number.MAX_SAFE_INTEGER }
-        ])('%j', ({ numerador }) => {
-            expect(calculadora.divisao(numerador, 0)).toEqual(Infinity);
-        });
-    });
+//     describe('o numerador é um número maior que 0 mas o denominador é igual a 0 ==> retorna Infinity', () => {
+//         test.each([
+//             { numerador: 6                      },
+//             { numerador: 2.1                    },
+//             { numerador: Number. MAX_VALUE      },
+//             { numerador: Number.MAX_SAFE_INTEGER }
+//         ])('%j', ({ numerador }) => {
+//             expect(calculadora.divisao(numerador, 0)).toEqual(Infinity);
+//         });
+//     });
 
-    describe('o numerador é um número menor que 0 mas o denominador é igual a 0 ==> retorna Infinity negativo', () => {
-        test.each([
-            { numerador: -6                      },
-            { numerador: -2.1                    },
-            { numerador: -Number. MAX_VALUE      },
-            { numerador: -Number.MAX_SAFE_INTEGER }
-        ])('%j', ({ numerador }) => {
-            expect(calculadora.divisao(numerador, 0)).toEqual(-Infinity);
-        });
-    });
+//     describe('o numerador é um número menor que 0 mas o denominador é igual a 0 ==> retorna Infinity negativo', () => {
+//         test.each([
+//             { numerador: -6                      },
+//             { numerador: -2.1                    },
+//             { numerador: -Number. MAX_VALUE      },
+//             { numerador: -Number.MAX_SAFE_INTEGER }
+//         ])('%j', ({ numerador }) => {
+//             expect(calculadora.divisao(numerador, 0)).toEqual(-Infinity);
+//         });
+//     });
 
-    describe('o numerador e o denominador são iguais a 0 ==> retorna NaN', () => {
-        test('%j', async () => {
-            expect(calculadora.divisao(0, 0)).toEqual(NaN);
-        });
-    });
+//     describe('o numerador e o denominador são iguais a 0 ==> retorna NaN', () => {
+//         test('%j', async () => {
+//             expect(calculadora.divisao(0, 0)).toEqual(NaN);
+//         });
+//     });
 
-    describe('algum dos parâmetros não é um número ==> lança exceção', () => {
-        test.each([        
-            { numerador: "uma string"     , denominador: 2.7666 },           
-            { numerador: true             , denominador: -2               },               
-            { numerador: { atributo: 1 }    , denominador: 4.2              },              
-            { numerador: () => {}         , denominador: Number. MAX_VALUE },
-            { numerador: 2.7666           , denominador: "uma string"     }, 
-            { numerador: -2               , denominador: true             },         
-            { numerador: 4.2              , denominador: { atributo: 1 }    },
-            { numerador: Number. MAX_VALUE, denominador: () => {}         }     
-        ])('%j', ({ numerador, denominador }) => {
-            expect(() => {
-                calculadora.divisao(numerador, denominador);
-            }).toThrow(TypeError);
-        });
-    });
-});
+//     describe('algum dos parâmetros não é um número ==> lança exceção', () => {
+//         test.each([        
+//             { numerador: "uma string"     , denominador: 2.7666 },           
+//             { numerador: true             , denominador: -2               },               
+//             { numerador: { atributo: 1 }    , denominador: 4.2              },              
+//             { numerador: () => {}         , denominador: Number. MAX_VALUE },
+//             { numerador: 2.7666           , denominador: "uma string"     }, 
+//             { numerador: -2               , denominador: true             },         
+//             { numerador: 4.2              , denominador: { atributo: 1 }    },
+//             { numerador: Number. MAX_VALUE, denominador: () => {}         }     
+//         ])('%j', ({ numerador, denominador }) => {
+//             expect(() => {
+//                 calculadora.divisao(numerador, denominador);
+//             }).toThrow(TypeError);
+//         });
+//     });
+// });
 
 describe('soma', () => {
     describe('2 números inteiros são passados como parâmetro ==> retorna a soma dos dois', () => {
